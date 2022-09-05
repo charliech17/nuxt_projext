ae5c1a3004cbb7cf53f36031f5f512e4c0701fff
kuo <charliech17@yahoo.com.tw>
2022-09-05 20:54:35 +0800

    add image fullScreen

---
+++ components/FullScreenView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div :class="{'img_section':isShowImg}">
+        <img 
+            src="https://source.unsplash.com/random/1600x900"
+            @click="isShowImg = !isShowImg"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isShowImg: false
+        }
+    },
+}
+</script>
+
+
+<style scoped>
+    .img_section{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <Tutorial/>
+  <div>
+      <Tutorial/>
+      <FullScreenView/>
+  </div>
 </template>
 
 <script>
