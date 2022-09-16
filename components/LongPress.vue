bb29b62af47ad1dba54485a6e8e2bc0c7bcd1ed9
kuo chuan <charliech17@yahoo.com.tw>
2022-09-16 13:58:47 +0800

    make button unselectable

--- components/LongPress.vue
+++ components/LongPress.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>長壓按鈕</h1>
-        <button @pointerdown="nowPointerPress" @pointerup="nowPointerUp">可測試長壓按鈕</button>
+        <button @pointerdown="nowPointerPress" @pointerup="nowPointerUp" class="removeSelect">可測試長壓按鈕</button>
         <div>
             <p :class="{'longPressText': isLongPress}">{{isLongPress ? '偵測到長壓按鈕' : '長壓上面按鈕，變更文字'}}</p>
             <button @click="isLongPress=false" tabindex="-1">清除</button>
@@ -36,4 +36,11 @@ export default {
         color: green;
     }
 
+    .removeSelect {
+        user-select: none;
+        -moz-user-select: none;
+        -khtml-user-select: none;
+        -webkit-user-select: none;
+        -o-user-select: none;
+    } 
 </style>
