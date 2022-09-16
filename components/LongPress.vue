fa1a2b3778e83f60b87db2ccd47172376b47edaa
kuo chuan <charliech17@yahoo.com.tw>
2022-09-16 13:55:37 +0800

    make button not focusable

--- components/LongPress.vue
+++ components/LongPress.vue
@@ -4,7 +4,7 @@
         <button @pointerdown="nowPointerPress" @pointerup="nowPointerUp">可測試長壓按鈕</button>
         <div>
             <p :class="{'longPressText': isLongPress}">{{isLongPress ? '偵測到長壓按鈕' : '長壓上面按鈕，變更文字'}}</p>
-            <button @click="isLongPress=false">清除</button>
+            <button @click="isLongPress=false" tabindex="-1">清除</button>
         </div>
     </div>
 </template>
@@ -35,7 +35,5 @@ export default {
     .longPressText {
         color: green;
     }
-
-</style>>
 
 </style>
