12a3e47c6a03718b42f24211efca7150affbb191
kuo chuan <charliech17@yahoo.com.tw>
2022-09-19 10:33:09 +0800

    change to width height 100%

--- components/FullScreenView.vue
+++ components/FullScreenView.vue
@@ -235,10 +235,12 @@ export default {
         position: fixed;
         top: 0;
         left: 0;
-        right: 0;
-        bottom: 0;
+        /* right: 0;
+        bottom: 0; */
         /* width: calc(var(--vw,1vw)*100);
         height: calc(var(--vh,1vh)*100); */
+        height: 100%;
+        width: 100%;
         background-color: black;
         overflow: hidden;
         z-index: 100;
