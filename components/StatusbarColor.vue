94e0e9d542eb5bfa3fa317bc17ee8d152e4a6e97
kuo <charliech17@yahoo.com.tw>
2022-09-20 23:30:46 +0800

    add function click color txt

--- components/StatusbarColor.vue
+++ components/StatusbarColor.vue
@@ -3,12 +3,15 @@
         <h1>更換Statusbar 顏色</h1>
         <input type="text" v-model="txtColor">
         <button @click="changeStatusbarColor">更變顏色</button>
-        <p>#FFFFFF</p>
-        <p>#000000</p>
+        <p @click="txtColor = '#FFFFFF'">#FFFFFF</p>
+        <div v-for="color in allColor" :key="color">
+            <p :style="{color: color}" @click="txtColor = color">{{color}}</p>
+        </div>
+        <!-- <p>#000000</p>
         <p style="color:#00FFFF">#00FFFF</p>
         <p style="color:#FF8C00">#FF8C00</p>
         <p style="color:#9932CC">#9932CC</p>
-        <p style="color:#8B0000">#8B0000</p>
+        <p style="color:#8B0000">#8B0000</p> -->
     </div>
 </template>
 
@@ -17,6 +20,7 @@ export default {
     data() {
         return {
             txtColor: "#000000",
+            allColor:['#000000','#00FFFF','#FF8C00','#9932CC','#8B0000']
         }
     },
     methods: {
