2ce05a274bc59a97a6b9cf6b3c709aaf985fef6d
kuo <charliech17@yahoo.com.tw>
2022-09-27 23:55:50 +0800

    long press focus add select

--- components/LongPress.vue
+++ components/LongPress.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
         <h1>長壓按鈕</h1>
-        <button @mousedown="nowPointerPress" @mouseup="nowPointerUp" class="removeSelect">可測試長壓按鈕</button>
+        <button @pointerdown="nowPointerPress" @pointerup="nowPointerUp" class="removeSelect">可測試長壓按鈕</button>
         <div>
             <p :class="{'longPressText': isLongPress}">{{isLongPress ? '偵測到長壓按鈕' : '長壓上面按鈕，變更文字'}}</p>
+            <div tabindex="-1" class="removeSelect" id="focusTxt" @pointerdown="nowPointerPress" @pointerup="nowPointerUp" @blur="removeFocus">我是要被長壓後複製的文字</div>
             <button @click="isLongPress=false" tabindex="-1">清除</button>
         </div>
     </div>
@@ -22,12 +23,30 @@ export default {
             // this.copyText()
             this.longPressTimer = setTimeout(() => {
                 this.isLongPress = true   
-                this.copyText()
+                const element = document.getElementById('focusTxt')
+
+                const selection = window.getSelection();
+                selection.removeAllRanges();
+
+                element.classList.remove('removeSelect')
+                // Select paragraph
+                const range = document.createRange();
+                range.selectNodeContents(element);
+                selection.addRange(range);
+
+
+                element.focus()
+                // const range = document.createRange()
+                // range.selectNode(element)
+
             },1200)
         },
         nowPointerUp() {
             clearTimeout(this.longPressTimer)
             this.longPressTimer = null
+        },
+        removeFocus() {
+            document.getElementById('focusTxt').classList.add('removeSelect')
         },
         async copyText() {
             const text = '我是被長壓複製的文字';
