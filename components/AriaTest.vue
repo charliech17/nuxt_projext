291922985b1b14bf5c3b9480a7443fd51fc08bd1
kuo <charliech17@yahoo.com.tw>
2022-10-12 23:36:01 +0800

    try aria

--- components/AriaTest.vue
+++ components/AriaTest.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
         <button @click="isOpenDrawer = true">開啟drawer</button>
-        <el-drawer title="我是標題" :visible.sync="isOpenDrawer" direction="btt">
+        <el-drawer 
+            :visible.sync="isOpenDrawer" 
+            title="我是標題" 
+            direction="btt"
+            role="dialog"
+            aria-labelledby="dialog1Title"
+            aria-describedby="dialog1Desc"
+        >
             <div role="heading">標題</div>
             <p>我是內文測試我是內文測試我是內文測試我是內文測試我是內文測試我是內文測試我是內文測試我是內文測試我是內文測試我是內文測試我是內文測試</p>
             <div role="button">確認</div>
