c1b19af982468316534887cedf97e227119e4ee2
Josh <josh_chang@JoshdeMacBook-Pro.local>
2022-11-30 15:06:16 +0800

    add focus and blur event

--- components/TestLeaveTime.vue
+++ components/TestLeaveTime.vue
@@ -14,12 +14,21 @@ export default {
         setInterval(()=>{
             this.countTime = this.countTime - 1  
         },1000)
+        // visibilitychange
         window.addEventListener("visibilitychange",()=>{
             if (document.visibilityState === 'visible') {
                 console.log('visible')
             } else {
                 console.log('unvisible')
             }
+        })
+        // window focus
+        window.addEventListener("focus",()=>{
+            console.log('window focus囉')
+        })
+        // window blur
+        window.addEventListener("blur",()=>{
+            console.log('blur囉！！！！')
         })
     }
 }
