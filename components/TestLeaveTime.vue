3195c2b430439f1fc6d0ec43d712696877deba29
Josh <josh_chang@JoshdeMacBook-Pro.local>
2023-01-18 10:20:26 +0800

    add testInterval

--- components/TestLeaveTime.vue
+++ components/TestLeaveTime.vue
@@ -7,11 +7,22 @@
         <button @click="handleTimeoutTest" style="background-color: #003ACD; color: white;">開始監聽 setTimeout</button>
         <div>{{ isNowSetTimeout }}</div>
         <div>{{ isErrorTime }}</div>
+        <button @click="handleTimeInterval_3min">測試三分鐘</button>
+        <div>{{ isTimeInterval_3min }}</div>
+        <div>{{ interval_3min_realTime }}</div>
+        <div>{{ interval_every_3min_realTime }}</div>
+        <button @click="handleTimeInterval_10min">測試十分鐘</button>
+        <div>{{ isTimeInterval_10min }}</div>
+        <div>{{ interval_10min_realTime }}</div>
+        <div>{{ interval_every_10min_realTime }}</div>
+        <button @click=""></button>
     </div>
 </template>
 
 
 <script>
+import { clearInterval } from 'timers'
+
 
 export default {
     data() {
@@ -19,6 +30,13 @@ export default {
             countTime: 100,
             isNowSetTimeout: '',
             isErrorTime: '',
+            isTimeInterval_3min: '',
+            isTimeInterval_10min: '',
+            interval_3min_realTime: '',
+            interval_10min_realTime: '',
+            interval_every_3min_realTime:'',
+            interval_every_10min_realTime:'',
+
         }
     },
     methods:{
@@ -55,7 +73,60 @@ export default {
                 const finalTime = new Date() 
                 console.log(finalTime - nowTime)
                 this.isErrorTime = finalTime - nowTime
-            },60000)
+            },600000)
+        },
+        handleTimeInterval_3min() {
+            this.isTimeInterval_3min = '三分鐘開始計時'
+            const nowDate = new Date()
+            let timeInter;
+
+            timeInter = setInterval(()=>{
+                const threeMinTime = new Date()
+                this.interval_3min_realTime = threeMinTime - nowDate
+                clearInterval(timeInter)
+            },180000)
+            
+            let timeInter_3min = 180000
+            let time_every_interval;
+            time_every_interval = setInterval(()=>{
+                if(timeInter_3min == 0) {
+                    timeInter_3min = timeInter_3min - 1000
+                    const threeFinalTime = new Date()
+                    this.interval_every_3min_realTime = threeFinalTime - nowDate
+                    clearInterval(time_every_interval)
+                } else if(timeInter_3min < 0) {
+                    return;
+                } else {
+                    timeInter_3min = timeInter_3min - 1000
+                }               
+            },1000)
+        },
+        handleTimeInterval_10min() {
+            this.isTimeInterval_10min =  '十分鐘開始計時'
+            const nowDate = new Date()
+            let timeInter;
+
+            timeInter = setInterval(()=>{
+                const tenMinTime = new Date()
+                this.interval_10min_realTime = tenMinTime - nowDate
+                clearInterval(timeInter)
+            },600000)
+
+            let timeInter_10min = 600000
+            let time_every_interval;
+            time_every_interval = setInterval(()=>{
+                if(timeInter_10min === 0) {
+                    timeInter_10min = timeInter_10min - 1000
+                    const tenFinalTime = new Date()
+                    this.interval_every_10min_realTime = tenFinalTime - nowDate
+                    clearInterval(time_every_interval)
+                } else if(timeInter_10min < 0) {
+                    return
+                }
+                 else {
+                    timeInter_10min = timeInter_10min - 1000
+                }               
+            },1000)
         },
     },
 }
