6f647af867dfe6c5623b1594e0d213778c14c7ff
Josh <kcchang1994@gmail.com>
2023-04-21 09:29:36 +0800

    add router page

---
+++ pages/testWhitePage2.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="scrollSection" id="scrollSectionID">
+        <div>
+            <h1>Router Page 1 ~~~</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aliquid eos eius maiores exercitationem quos atque quibusdam odio nobis voluptate illo doloribus asperiores fugiat, quas est, architecto velit sed quae.
+            Sequi fuga alias perferendis accusantium, ipsam adipisci necessitatibus odit, vero, eaque obcaecati dolores! Molestiae, nesciunt explicabo! Nesciunt ad similique, nisi asperiores ipsum magni hic, labore fugit dolorum necessitatibus soluta laudantium?
+            Temporibus, nam. Sunt sint cumque vitae totam eligendi quis adipisci doloremque ratione vero veniam tenetur nesciunt maiores sequi beatae qui quo quia, ut eius dignissimos aut natus dolorem id! Eos?
+            Vel nihil, rerum ducimus consequuntur debitis tenetur cumque amet necessitatibus eos nesciunt quaerat molestias omnis voluptatem illum eveniet impedit rem cum? Accusantium ad nostrum omnis aspernatur pariatur non cum vitae?
+            Velit quis earum dolor obcaecati fuga repellat assumenda nisi fugiat? Repudiandae, libero quae fuga itaque sed inventore consequatur incidunt laudantium animi tempore, necessitatibus consequuntur omnis laboriosam perspiciatis saepe quia culpa?
+            Labore, repellendus totam, veritatis asperiores minima consequatur soluta in impedit libero itaque quasi incidunt similique ipsa exercitationem earum corrupti officia neque, eius sint sed maiores illum quos. Incidunt, ut cum?
+            Illo eius nostrum veritatis iste voluptatum dolores corporis deleniti. Libero, ab qui consequuntur quam fuga placeat, repellat nemo illo officia culpa eum incidunt dolor vel veniam suscipit harum velit saepe.
+            Quisquam architecto quod nobis maiores, doloremque non, consequuntur molestias iste incidunt officiis facere ipsa aperiam veritatis fugiat autem illo qui est at eius tempore, nostrum soluta facilis inventore. Perspiciatis, ad?
+            Ipsam perspiciatis dolor voluptas neque, nihil quos, eos consectetur cumque iure quia, sunt quibusdam earum illum deserunt voluptates alias porro quae corrupti! Rem consequuntur, impedit incidunt magni expedita recusandae error.
+            Sunt, eligendi blanditiis laudantium quaerat nobis doloribus accusamus, ratione officia, possimus voluptate nihil eius hic repellat laboriosam eveniet nostrum! Sit et quidem quia maiores exercitationem commodi quas voluptates quos atque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aliquid eos eius maiores exercitationem quos atque quibusdam odio nobis voluptate illo doloribus asperiores fugiat, quas est, architecto velit sed quae.
+            Sequi fuga alias perferendis accusantium, ipsam adipisci necessitatibus odit, vero, eaque obcaecati dolores! Molestiae, nesciunt explicabo! Nesciunt ad similique, nisi asperiores ipsum magni hic, labore fugit dolorum necessitatibus soluta laudantium?
+            Temporibus, nam. Sunt sint cumque vitae totam eligendi quis adipisci doloremque ratione vero veniam tenetur nesciunt maiores sequi beatae qui quo quia, ut eius dignissimos aut natus dolorem id! Eos?
+            Vel nihil, rerum ducimus consequuntur debitis tenetur cumque amet necessitatibus eos nesciunt quaerat molestias omnis voluptatem illum eveniet impedit rem cum? Accusantium ad nostrum omnis aspernatur pariatur non cum vitae?
+            Velit quis earum dolor obcaecati fuga repellat assumenda nisi fugiat? Repudiandae, libero quae fuga itaque sed inventore consequatur incidunt laudantium animi tempore, necessitatibus consequuntur omnis laboriosam perspiciatis saepe quia culpa?
+            Labore, repellendus totam, veritatis asperiores minima consequatur soluta in impedit libero itaque quasi incidunt similique ipsa exercitationem earum corrupti officia neque, eius sint sed maiores illum quos. Incidunt, ut cum?
+            Illo eius nostrum veritatis iste voluptatum dolores corporis deleniti. Libero, ab qui consequuntur quam fuga placeat, repellat nemo illo officia culpa eum incidunt dolor vel veniam suscipit harum velit saepe.
+            Quisquam architecto quod nobis maiores, doloremque non, consequuntur molestias iste incidunt officiis facere ipsa aperiam veritatis fugiat autem illo qui est at eius tempore, nostrum soluta facilis inventore. Perspiciatis, ad?
+            Ipsam perspiciatis dolor voluptas neque, nihil quos, eos consectetur cumque iure quia, sunt quibusdam earum illum deserunt voluptates alias porro quae corrupti! Rem consequuntur, impedit incidunt magni expedita recusandae error.
+            Sunt, eligendi blanditiis laudantium quaerat nobis doloribus accusamus, ratione officia, possimus voluptate nihil eius hic repellat laboriosam eveniet nostrum! Sit et quidem quia maiores exercitationem commodi quas voluptates quos atque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aliquid eos eius maiores exercitationem quos atque quibusdam odio nobis voluptate illo doloribus asperiores fugiat, quas est, architecto velit sed quae.
+            Sequi fuga alias perferendis accusantium, ipsam adipisci necessitatibus odit, vero, eaque obcaecati dolores! Molestiae, nesciunt explicabo! Nesciunt ad similique, nisi asperiores ipsum magni hic, labore fugit dolorum necessitatibus soluta laudantium?
+            Temporibus, nam. Sunt sint cumque vitae totam eligendi quis adipisci doloremque ratione vero veniam tenetur nesciunt maiores sequi beatae qui quo quia, ut eius dignissimos aut natus dolorem id! Eos?
+            Vel nihil, rerum ducimus consequuntur debitis tenetur cumque amet necessitatibus eos nesciunt quaerat molestias omnis voluptatem illum eveniet impedit rem cum? Accusantium ad nostrum omnis aspernatur pariatur non cum vitae?
+            Velit quis earum dolor obcaecati fuga repellat assumenda nisi fugiat? Repudiandae, libero quae fuga itaque sed inventore consequatur incidunt laudantium animi tempore, necessitatibus consequuntur omnis laboriosam perspiciatis saepe quia culpa?
+            Labore, repellendus totam, veritatis asperiores minima consequatur soluta in impedit libero itaque quasi incidunt similique ipsa exercitationem earum corrupti officia neque, eius sint sed maiores illum quos. Incidunt, ut cum?
+            Illo eius nostrum veritatis iste voluptatum dolores corporis deleniti. Libero, ab qui consequuntur quam fuga placeat, repellat nemo illo officia culpa eum incidunt dolor vel veniam suscipit harum velit saepe.
+            Quisquam architecto quod nobis maiores, doloremque non, consequuntur molestias iste incidunt officiis facere ipsa aperiam veritatis fugiat autem illo qui est at eius tempore, nostrum soluta facilis inventore. Perspiciatis, ad?
+            Ipsam perspiciatis dolor voluptas neque, nihil quos, eos consectetur cumque iure quia, sunt quibusdam earum illum deserunt voluptates alias porro quae corrupti! Rem consequuntur, impedit incidunt magni expedita recusandae error.
+            Sunt, eligendi blanditiis laudantium quaerat nobis doloribus accusamus, ratione officia, possimus voluptate nihil eius hic repellat laboriosam eveniet nostrum! Sit et quidem quia maiores exercitationem commodi quas voluptates quos atque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aliquid eos eius maiores exercitationem quos atque quibusdam odio nobis voluptate illo doloribus asperiores fugiat, quas est, architecto velit sed quae.
+            Sequi fuga alias perferendis accusantium, ipsam adipisci necessitatibus odit, vero, eaque obcaecati dolores! Molestiae, nesciunt explicabo! Nesciunt ad similique, nisi asperiores ipsum magni hic, labore fugit dolorum necessitatibus soluta laudantium?
+            Temporibus, nam. Sunt sint cumque vitae totam eligendi quis adipisci doloremque ratione vero veniam tenetur nesciunt maiores sequi beatae qui quo quia, ut eius dignissimos aut natus dolorem id! Eos?
+            Vel nihil, rerum ducimus consequuntur debitis tenetur cumque amet necessitatibus eos nesciunt quaerat molestias omnis voluptatem illum eveniet impedit rem cum? Accusantium ad nostrum omnis aspernatur pariatur non cum vitae?
+            Velit quis earum dolor obcaecati fuga repellat assumenda nisi fugiat? Repudiandae, libero quae fuga itaque sed inventore consequatur incidunt laudantium animi tempore, necessitatibus consequuntur omnis laboriosam perspiciatis saepe quia culpa?
+            Labore, repellendus totam, veritatis asperiores minima consequatur soluta in impedit libero itaque quasi incidunt similique ipsa exercitationem earum corrupti officia neque, eius sint sed maiores illum quos. Incidunt, ut cum?
+            Illo eius nostrum veritatis iste voluptatum dolores corporis deleniti. Libero, ab qui consequuntur quam fuga placeat, repellat nemo illo officia culpa eum incidunt dolor vel veniam suscipit harum velit saepe.
+            Quisquam architecto quod nobis maiores, doloremque non, consequuntur molestias iste incidunt officiis facere ipsa aperiam veritatis fugiat autem illo qui est at eius tempore, nostrum soluta facilis inventore. Perspiciatis, ad?
+            Ipsam perspiciatis dolor voluptas neque, nihil quos, eos consectetur cumque iure quia, sunt quibusdam earum illum deserunt voluptates alias porro quae corrupti! Rem consequuntur, impedit incidunt magni expedita recusandae error.
+            Sunt, eligendi blanditiis laudantium quaerat nobis doloribus accusamus, ratione officia, possimus voluptate nihil eius hic repellat laboriosam eveniet nostrum! Sit et quidem quia maiores exercitationem commodi quas voluptates quos atque.
+        </div>
+        <div class="bottomBtnStyle">
+            <div class="whitePageBtn" @click="()=>navigateWhitePage(true)">Ios滑動時點我實現白頁效果</div>
+            <div class="normalPageBtn" @click="() => navigateWhitePage(false)">Ios滑動時點擊無白頁效果</div>
+        </div>
+    </div>
+</template>
+
+<script>
+ export default{
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+        const vh = window.innerHeight / 100
+        document.documentElement.style.setProperty('--vh',`${vh}px`)
+    },
+    methods: {
+        navigateWhitePage(isShowWhitePage) {
+            if(isShowWhitePage) {
+                this.$router.push('/testWhitePage3')
+            } else {
+                document.getElementById('scrollSectionID').style.overflow = 'hidden'
+                this.$nextTick(()=> {
+                    this.$router.push('/testWhitePage3')
+                })
+            }
+        }
+    }
+ }
+</script>
+
+<style scoped>
+.scrollSection{
+    height: calc( var(--vh,1vh)*100 - 100px - 16px);
+    overflow: auto;
+}
+
+.bottomBtnStyle{
+    position: fixed; 
+    bottom: 0;
+    height: 100px; 
+    width: calc(100vw - 8px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: -4px;
+}
+
+.bottomBtnStyle > div{
+    line-height: 50px;
+}
+
+.changePageBtn{
+    flex:1;
+    background-color: rgb(147, 43, 138);
+    color: white;
+    width: 100%;
+    text-align: center;
+    border: 1px solid black;
+    border-bottom: none;
+}
+
+.whitePageBtn{
+    flex:1;
+    background-color: rgb(72, 35, 69);
+    color: white;
+    width: 100%;
+    text-align: center;
+    border: 1px solid black;
+    border-bottom: none;
+}
+
+.normalPageBtn{
+    flex:1;
+    width: 100%;
+    text-align: center;
+    border: 1px solid black;
+}
+
+.normalPageBtn:hover,
+.whitePageBtn:hover{
+    background-color: rgb(183, 147, 218);
+    color: white;
+}
+</style>
