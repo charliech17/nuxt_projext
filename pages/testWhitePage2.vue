56593c3de8247d4d2ad02aa4533e77ed37bb534b
Josh <kcchang1994@gmail.com>
2023-04-21 10:28:08 +0800

    add

--- pages/testWhitePage2.vue
+++ pages/testWhitePage2.vue
@@ -59,8 +59,9 @@
     },
     mounted() {
         this.$nextTick(()=> {
+            document.getElementById('scrollSectionID').scrollTop = 0
             if(localStorage['needResetScroll']) {
-                document.getElementById('scrollSectionID').style.overflow = 'auto'
+                document.getElementById('scrollSectionID').style.overflow = ''
                 localStorage.removeItem('needResetScroll')
             }
         })
