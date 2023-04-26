8d15be986ec648ced9f6468d14dd0531c6ddd644
Josh <kcchang1994@gmail.com>
2023-04-26 08:58:48 +0800

    test in app browser

---
+++ pages/inAppBrowser.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>是否在app Browser <span style="color: blue">{{ judgeIsinApp }}</span></h1>
+        <p>{{ nowBrowser }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                nowBrowser: ''
+            }
+        },
+        computed: {
+            judgeIsinApp() {
+                let isInAppBrowser = false
+
+                const BROWSER = {
+                    messenger: /\bFB[\w_]+\/(Messenger|MESSENGER)/,
+                    facebook: /\bFB[\w_]+\//,
+                    twitter: /\bTwitter/i,
+                    line: /\bLine\//i,
+                    instagram: /\bInstagram/i,
+                };
+
+                const useragent = navigator.userAgent;
+                this.nowBrowser = useragent
+
+                for(const eachBrowser in BROWSER) {
+                    if(useragent.match(BROWSER[eachBrowser])) {
+                        isInAppBrowser = true
+                        return
+                    }
+                }
+
+                return isInAppBrowser
+            }
+        }
+    }
+</script>
