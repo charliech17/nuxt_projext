a72e4a9275c1e6b80e2d61d1a5da3fddbcd79ee4
kuo <charliech17@yahoo.com.tw>
2023-06-26 13:44:55 +0800

    修正pdf錯誤問題(要用base64開)

--- components/PdfMutiPage.vue
+++ components/PdfMutiPage.vue
@@ -1,9 +1,15 @@
 <template>
     <div>
-        <vue-pdf-app 
-            style="height: 100vh;" 
-            :pdf="'@/assets/pdf/test2.pdf'"
-        />
+        <input id="inputFile" type="file" @change="convertToBase64" />
+        <div v-if="isShowPDF" @pointerdown="handlePDF_touch">
+            <vue-pdf-app 
+                v-if="isShowPDF"
+                style="height: 40vh;" 
+                :pdf="pdf"
+                :page-number="1"
+                :file-name="filename"
+            />
+        </div>
     </div>
 </template>
 
@@ -13,6 +19,42 @@
     export default {
         components: {
             VuePdfApp
+        },
+        data() {
+            return {
+                pdf: '',
+                filename: '',
+                isShowPDF: false
+            }
+        },
+        methods: {
+            convertToBase64() {
+                //Read File
+                const selectedFile = document.getElementById("inputFile").files;
+                //Check File is not Empty
+                if (selectedFile.length > 0) {
+                    // Select the very first file from list
+                    const fileToLoad = selectedFile[0];
+                    // FileReader function for read the file.
+                    const fileReader = new FileReader();
+                    let base64;
+
+                    const vueThis = this
+                    // Onload of file read the file content
+                    fileReader.onload = function(fileLoadedEvent) {
+                        base64 = fileLoadedEvent.target.result;
+
+                        vueThis.pdf = base64
+                        vueThis.fileName = new Date()
+                        vueThis.isShowPDF = true
+                    };
+                    // Convert data to base64
+                    fileReader.readAsDataURL(fileToLoad);
+                }
+            },
+            handlePDF_touch() {
+                console.log('pdf touch')
+            }
         }
     };
 </script>
