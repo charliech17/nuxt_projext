a1d195d5c7458863019f2af209b76e7a5b5eb603
Josh <kcchang1994@gmail.com>
2023-07-06 14:02:53 +0800

    新增imageViewer

---
+++ pages/testImageViewer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    import '@/imageView/image_viewer.js'
+    
+    export default {
+        mounted() {
+            const prefix = '@/assets/imageViewer'
+            // console.log(require(`@/assets/imageViewer/1.png`))
+            const imgUrls =  [require(`@/assets/imageViewer/1.png`),require(`@/assets/imageViewer/2.png`),require(`@/assets/imageViewer/3.jpeg`)]
+            const currentUrl = require(`@/assets/imageViewer/1.png`)
+            const obj = {
+                urls : imgUrls,
+                current : currentUrl
+            };
+            setTimeout(()=> {
+                previewImage.start(obj);
+            })
+        }
+    }
+</script>
